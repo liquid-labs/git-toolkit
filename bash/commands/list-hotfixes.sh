ea2c21cb50e26405f480231211085db0a7d04162
Zane Rockenbaugh <zane@liquid-labs.com>
2020-09-10 20:22:50 -0500

    minimum script

--- bash/commands/list-hotfixes.sh
+++ bash/commands/list-hotfixes.sh
@@ -1,3 +1,4 @@
 function list-hotfixes {
-  echo "TODO: implement me"
+  # TODO: implement '--no-color' (from global option?)
+  git log --no-merges --first-parent --pretty=oneline --decorate=full --min-parents=1 --color | grep --color=always -Fv 'tag: refs/tags'
 }
