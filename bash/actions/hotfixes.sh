10a772b98f6f73e3238e11de0fb81c75b9dcdeba
Zane Rockenbaugh <zane@liquid-labs.com>
2020-09-17 19:01:39 -0500

    space change?

--- bash/actions/hotfixes.sh
+++ bash/actions/hotfixes.sh
@@ -8,7 +8,7 @@ function gtk-hotfixes() {
   fi
 }
 
-gtk-hotfixes-list() {  
+gtk-hotfixes-list() {
   # TODO: implement '--no-color' (from global option?)
   git log --no-merges --first-parent --pretty=oneline --decorate=full --min-parents=1 --color | grep --color=always -Fv 'tag: refs/tags'
 }
