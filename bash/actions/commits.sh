dc4486140f0c374f7ec4ac4b4c1d32efb67c31a6
Zane Rockenbaugh <zane@liquid-labs.com>
2020-09-17 23:26:14 -0500

    iterative step to unify commit output

--- bash/actions/commits.sh
+++ bash/actions/commits.sh
@@ -37,6 +37,14 @@ gtk-commits-list() {
     fi
     # TODO: implement '--no-color|-C' (from global option?)
     git log --no-merges --first-parent --pretty=oneline --decorate=full --min-parents=1 --color $RANGE | grep --color=always -Fv 'tag: refs/tags'
+    echo -e "\n------\n"
+    # git log --no-merges --first-parent --color --pretty=format:'+%C(bold 214)%<(7,trunc)%h%C(reset)+%C(dim white)%>(12,trunc)%cr%C(reset)+%C(214)%>(15,trunc)%d%C(reset)+%C(white)%s%C(reset)' --min-parents=1 ${RANGE} -- | grep --color=always -Fv 'tag: refs/tags'
+    echo "git log --no-merges --first-parent --min-parents=1 --color --pretty=format:'+%C(bold 214)%<(7,trunc)%h%C(reset) %s' ${RANGE} -- | grep --color=always -FV 'tag: refs/tags'"
+    # --no-merges : excludes the workbranch merges
+    # --first-parent says to follow the 'master' branch and don't walk into merge branches
+    # --min-parent=1 : excludes the initial commit, if any; that's not a hotfix.
+    # --decorate=full : says to print the full refspec with '%d' so we can filter it (the tags) out
+    git log --no-merges --first-parent --min-parents=1 --color --decorate=full --pretty=format:'%C(bold 214)%<(7,trunc)%h %C(dim white)%>(12,trunc)%cr%C(reset) %d%s' ${RANGE} -- | grep --color=always -Fv 'tag: refs/tags'
   else
     echoerrandexit "No such content type: ${CONTENT}. ${green}Use smart autocomplete or try:\ngtk help list${reset}"
   fi
