00253fb237ff88fb7ff546a5de52dab4525a0a24
Zane Rockenbaugh <zane@liquid-labs.com>
2023-02-11 18:18:55 -0600

    fix some typo errors

--- src/lib/try-exec.mjs
+++ src/lib/try-exec.mjs
@@ -1,10 +1,11 @@
+import createError from 'http-errors'
 import shell from 'shelljs'
 
 const tryExec = (cmd, { httpStatus = 500, msg = '', msgFunc } = {}) => {
   const result = shell.exec(cmd, { silent: true })
   if (result.code !== 0) {
     if (msg.length > 0) msg += ' '
-    if (msfFunc !== undefined) msg += msgFunc(result) + ' '
+    if (msgFunc !== undefined) msg += msgFunc(result) + ' '
 
     throw createError(httpStatus, msg + `Failed to execute '${cmd}'; stderr: ${result.stderr}`)
   }
